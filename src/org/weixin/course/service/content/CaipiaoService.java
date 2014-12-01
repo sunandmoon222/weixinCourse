deb8f2e530b9b49c7c61f594ee38faf1ad4bc29b
sunandmoon222 <41676384@qq.com>
2014-12-01 18:00:58 +0800

    20141201

---
+++ src/org/weixin/course/service/content/CaipiaoService.java
@@ -0,0 +1,190 @@
+package org.weixin.course.service.content;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.weixin.course.service.content.bean.DaletouBean;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+/**
+ * 彩票查询服务
+ * 
+ * @author zhangtianming
+ * @date 2014-12-01
+ * 
+ */
+public class CaipiaoService {
+
+	private List<DaletouBean> dataList = new ArrayList<DaletouBean>();
+	private final static String filePath = System.getProperty("user.dir") + "\\src\\Resources\\data\\caipiao\\daletou.xml";
+	private final static String filePath_1 = System.getProperty("user.dir") + "\\src\\Resources\\data\\caipiao\\daletou_new.xml";
+	private final int pageNum = 24;
+	
+	/**
+	 * 发起http get请求获取网页源代码
+	 * 
+	 * @param requestUrl
+	 * @return
+	 */
+	private String httpRequest(String requestUrl) {
+		StringBuffer buffer = null;
+
+		try {
+			// 建立连接
+			URL url = new URL(requestUrl);
+			HttpURLConnection httpUrlConn = (HttpURLConnection) url
+					.openConnection();
+			httpUrlConn.setDoInput(true);
+			httpUrlConn.setRequestMethod("GET");
+
+			// 获取输入流
+			InputStream inputStream = httpUrlConn.getInputStream();
+			InputStreamReader inputStreamReader = new InputStreamReader(
+					inputStream, "utf-8");
+			BufferedReader bufferedReader = new BufferedReader(
+					inputStreamReader);
+
+			// 读取返回结果
+			buffer = new StringBuffer();
+			String str = null;
+			while ((str = bufferedReader.readLine()) != null) {
+				buffer.append(str);
+			}
+
+			// 释放资源
+			bufferedReader.close();
+			inputStreamReader.close();
+			inputStream.close();
+			httpUrlConn.disconnect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return buffer.toString();
+	}
+
+	/**
+	 * 从html中抽取出彩票数据
+	 * 
+	 * @param html
+	 * @return
+	 */
+	private void extract(String html,int num) {
+
+		String strTemp = null;
+		Pattern p = Pattern.compile("(.*)(追加奖金</td>\\s+</tr>)(.*)</TABLE>.*");
+//Pattern p = Pattern.compile("(.*)(<tr align=\"center\" bgcolor=\"#ffffff\">\\s+<td height=\"24\">\\s+" + String.valueOf(count - (num-1)*50) +"\\s+</td>)(.*)</TABLE>.*");
+//Pattern.compile("(.*)(<tr align=\"center\" bgcolor=\"#ffffff\">)(.*)</TABLE>.*");
+		Matcher m = p.matcher(html);
+		while (m.find()) {
+			strTemp = m.group(3);
+		}
+
+		String[] data = strTemp.split("</tr>");
+
+		for (int i = 0; i < data.length - 1; i++) {
+
+			DaletouBean bean = new DaletouBean();
+			String dataTemp = data[i].replace("    ", "")
+									 .replaceAll("</?[^>]+>", "&").replaceAll("&+", "&")
+									 .replaceAll("&\\s\\+\\s&", "&");
+			String[] dataTemp1 = dataTemp.substring(1, dataTemp.length() - 1).split("&");
+			bean.setId(dataTemp1[0]);
+			bean.setResultNum_red(dataTemp1[1]);
+			bean.setResultNum_blue(dataTemp1[2]);
+			bean.setWinningNum(dataTemp1[3]);
+			bean.setBonusAmount(dataTemp1[4]);
+			bean.setWinningNumAdd(dataTemp1[5]);
+			bean.setBonusAmountAdd(dataTemp1[6]);
+			bean.setWinningNum_2(dataTemp1[7]);
+			bean.setBonusAmount_2(dataTemp1[8]);
+			bean.setWinningNumAdd_2(dataTemp1[9]);
+			bean.setBonusAmountAdd_2(dataTemp1[10]);
+			bean.setOpenTime(dataTemp1[11]);
+
+			dataList.add(bean);
+		}
+
+	}
+
+	private void makeXmlData() {
+
+		XStream xs = new XStream();
+		// Write to a file in the file system
+		try {
+			OutputStream fs = new FileOutputStream(filePath);
+			OutputStream fs1 = new FileOutputStream(filePath_1);
+			xs.toXML(dataList, fs);
+			xs.toXML(dataList.get(0), fs1);
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	/**
+	 * 彩票查询方法，供外部调用
+	 * 
+	 * @return
+	 */
+	public void makeCaipiaoInfo() {
+		
+		// 获取网页源代码
+		for (int i = 1; i < pageNum; i++) {
+			
+			String html = httpRequest("http://www.lottery.gov.cn/lottery/dlt/History.aspx?p="+String.valueOf(i));
+			// 从网页中抽取信息
+			extract(html,i);
+			try {
+				Thread.sleep(2 * 1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			System.out.println("i == " +i);
+		}
+
+		makeXmlData();
+	}
+
+	public static String getTodayDate() {
+		
+		XStream xs = new XStream(new DomDriver());
+		DaletouBean bean = new DaletouBean();
+		File file = new File(filePath_1);
+		InputStreamReader input = null;
+		try {
+			input = new FileReader(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		xs.alias("daletou", CaipiaoService.class);
+		xs.fromXML(input, bean);
+		return null;
+	}
+	
+	/**
+	 * 通过main在本地测试
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		getTodayDate();
+//		CaipiaoService caipiaoService = new CaipiaoService();
+//		caipiaoService.makeCaipiaoInfo();
+		System.out.print("success~~~");
+	}
+}
---
+++ src/org/weixin/course/service/content/bean/DaletouBean.java
@@ -0,0 +1,131 @@
+package org.weixin.course.service.content.bean;
+
+
+public class DaletouBean extends CaipiaoBaseBean {
+
+	//一等奖追加中奖数目
+	private String winningNumAdd = "";
+	//一等奖追加中奖金额
+	private String bonusAmountAdd = "";
+	//二等奖中奖数目
+	private String winningNum_2 = "";
+	//二等奖中奖金额
+	private String bonusAmount_2 = "";
+	//二等奖追加中奖数目
+	private String winningNumAdd_2 = "";
+	//二等奖追加中奖金额
+	private String bonusAmountAdd_2 = "";
+	//开奖号码 红号
+	private String resultNum_red = "";
+	//开奖号码 蓝号
+	private String resultNum_blue = "";	
+	
+	/**
+	 * @return the resultNum_red
+	 */
+	public String getResultNum_red() {
+		return resultNum_red;
+	}
+
+	/**
+	 * @param resultNum_red the resultNum_red to set
+	 */
+	public void setResultNum_red(String resultNum_red) {
+		this.resultNum_red = resultNum_red;
+	}
+
+	/**
+	 * @return the resultNum_blue
+	 */
+	public String getResultNum_blue() {
+		return resultNum_blue;
+	}
+
+	/**
+	 * @param resultNum_blue the resultNum_blue to set
+	 */
+	public void setResultNum_blue(String resultNum_blue) {
+		this.resultNum_blue = resultNum_blue;
+	}
+
+	public String getWinningNumAdd() {
+		return winningNumAdd;
+	}
+
+	/**
+	 * @param winningNumAdd the winningNumAdd to set
+	 */
+	public void setWinningNumAdd(String winningNumAdd) {
+		this.winningNumAdd = winningNumAdd;
+	}
+
+	/**
+	 * @return the bonusAmountAdd
+	 */
+	public String getBonusAmountAdd() {
+		return bonusAmountAdd;
+	}
+
+	/**
+	 * @param bonusAmountAdd the bonusAmountAdd to set
+	 */
+	public void setBonusAmountAdd(String bonusAmountAdd) {
+		this.bonusAmountAdd = bonusAmountAdd;
+	}
+
+	/**
+	 * @return the winningNum_2
+	 */
+	public String getWinningNum_2() {
+		return winningNum_2;
+	}
+
+	/**
+	 * @param winningNum_2 the winningNum_2 to set
+	 */
+	public void setWinningNum_2(String winningNum_2) {
+		this.winningNum_2 = winningNum_2;
+	}
+
+	/**
+	 * @return the bonusAmount_2
+	 */
+	public String getBonusAmount_2() {
+		return bonusAmount_2;
+	}
+
+	/**
+	 * @param bonusAmount_2 the bonusAmount_2 to set
+	 */
+	public void setBonusAmount_2(String bonusAmount_2) {
+		this.bonusAmount_2 = bonusAmount_2;
+	}
+
+	/**
+	 * @return the winningNumAdd_2
+	 */
+	public String getWinningNumAdd_2() {
+		return winningNumAdd_2;
+	}
+
+	/**
+	 * @param winningNumAdd_2 the winningNumAdd_2 to set
+	 */
+	public void setWinningNumAdd_2(String winningNumAdd_2) {
+		this.winningNumAdd_2 = winningNumAdd_2;
+	}
+
+	/**
+	 * @return the bonusAmountAdd_2
+	 */
+	public String getBonusAmountAdd_2() {
+		return bonusAmountAdd_2;
+	}
+
+	/**
+	 * @param bonusAmountAdd_2 the bonusAmountAdd_2 to set
+	 */
+	public void setBonusAmountAdd_2(String bonusAmountAdd_2) {
+		this.bonusAmountAdd_2 = bonusAmountAdd_2;
+	}
+}
