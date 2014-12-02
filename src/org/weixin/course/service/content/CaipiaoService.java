7ae3304ea43509b5111271ea5b225e384210ec00
sunandmoon222 <41676384@qq.com>
2014-12-02 19:06:54 +0800

    20141202 modify

--- src/org/weixin/course/service/content/CaipiaoService.java
+++ src/org/weixin/course/service/content/CaipiaoService.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.weixin.course.service.content.bean.DaletouBean;
+import org.weixin.course.service.content.bean.PailiesanBean;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
@@ -21,15 +22,17 @@ import com.thoughtworks.xstream.io.xml.DomDriver;
 public class CaipiaoService {
 
 //	private final static String filePath = System.getProperty("user.dir") + "\\src\\Resources\\data\\caipiao\\daletou_new.xml";
-	private final static String filePath = "/alidata/server/tomcat7/webapps/weixinCourse/WEB-INF/classes/Resources/data/caipiao/daletou_new.xml";
-	private final static String appName = "超级大乐透"; 
+	private final static String daletou_filePath = "/alidata/server/tomcat7/webapps/weixinCourse/WEB-INF/classes/Resources/data/caipiao/daletou_new.xml";
+	private final static String pailiesan_filePath = "/alidata/server/tomcat7/webapps/weixinCourse/WEB-INF/classes/Resources/data/caipiao/pailiesan_new.xml";
+	private final static String appName_daletou = "超级大乐透";
+	private final static String appName_pailiesan = "排列三"; 
 	
 	private static String getDaletouInfo() {
 		StringBuffer buffer = new StringBuffer();
 		DaletouBean bean = new DaletouBean();
 		XStream xs = new XStream(new DomDriver());
 		
-		File file = new File(filePath);
+		File file = new File(daletou_filePath);
 		InputStream input = null;
 		try {
 			input = new FileInputStream(file);
@@ -40,14 +43,42 @@ public class CaipiaoService {
 		
 		xs.fromXML(input, bean);
 		
-		buffer.append(appName+"(期号:"+bean.getId()+")").append("\n")
+		buffer.append(appName_daletou+"(期号:"+bean.getId()+")").append("\n")
 			  .append("中奖号码：").append("\n")
 			  .append("红球：").append(bean.getResultNum_red()).append("\n")
 			  .append("蓝球：").append(bean.getResultNum_blue()).append("\n")
 			  .append("一等奖中奖人数：").append(bean.getWinningNum()).append("\n")
-			  .append("一等奖中奖金额：").append(bean.getBonusAmount()).append("\n")
-			  .append("二等奖中奖人数：").append(bean.getWinningNum_2()).append("\n")
-			  .append("二等奖中奖金额：").append(bean.getBonusAmount_2()).append("\n");
+			  .append("一等奖中奖金额：").append(bean.getBonusAmount()).append("\n");
+//			  .append("二等奖中奖人数：").append(bean.getWinningNum_2()).append("\n")
+//			  .append("二等奖中奖金额：").append(bean.getBonusAmount_2()).append("\n");
+
+		try {
+			input.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		input = null;
+		return buffer.toString();
+	}
+	
+	private static String getPaiLieSanInfo() {
+		StringBuffer buffer = new StringBuffer();
+		PailiesanBean bean = new PailiesanBean();
+		XStream xs = new XStream(new DomDriver());
+		
+		File file = new File(pailiesan_filePath);
+		InputStream input = null;
+		try {
+			input = new FileInputStream(file);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} 
+		
+		xs.fromXML(input, bean);
+		
+		buffer.append(appName_pailiesan+"(期号:"+bean.getId()+")").append("\n")
+			  .append("中奖号码：").append(bean.getResultNum()).append("\n");
 
 		try {
 			input.close();
@@ -61,9 +92,11 @@ public class CaipiaoService {
 	public static String getCaipiaoInfo() {
 		
 		String strDaletou = getDaletouInfo();
+		String strPaiLieSan = getPaiLieSanInfo();
 		
 		StringBuffer buffer = new StringBuffer();
-		buffer.append(strDaletou);
+		buffer.append(strDaletou).append("\n\n")
+			  .append(strPaiLieSan);
 
 		return buffer.toString();
 	}
